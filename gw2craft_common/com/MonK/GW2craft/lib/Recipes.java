edb62d81c589f4245ddaa749c5c0f37c7d83aac3
ooMonkoo <oomonkoo@gmx.net>
2013-09-22 17:09:51 +0200

    Added some first Basic Smelting Recipes..Only Placeholder for the Crafting Stations

--- gw2craft_common/com/MonK/GW2craft/lib/Recipes.java
+++ gw2craft_common/com/MonK/GW2craft/lib/Recipes.java
@@ -1,25 +1,40 @@
 package com.MonK.GW2craft.lib;
 
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.FurnaceRecipes;
 
-import com.MonK.GW2craft.blocks.Blocks;
 import com.MonK.GW2craft.items.Items;
+
+import cpw.mods.fml.common.registry.GameRegistry;
 
 public class Recipes {
     
     public static void init() {
         
-        FurnaceRecipes.smelting().addSmelting(Blocks.oreCopper.blockID, new ItemStack(Items.oreCopperItem), 0.1F);
-        FurnaceRecipes.smelting().addSmelting(Blocks.oreIron.blockID, new ItemStack(Items.oreIronItem), 0.1F);
-        FurnaceRecipes.smelting().addSmelting(Blocks.oreSilver.blockID, new ItemStack(Items.oreSilverItem), 0.1F);
-        FurnaceRecipes.smelting().addSmelting(Blocks.oreGold.blockID, new ItemStack(Items.oreGoldItem), 0.1F);
-        FurnaceRecipes.smelting().addSmelting(Blocks.orePlatinum.blockID, new ItemStack(Items.orePlatinumItem), 0.1F);
-        FurnaceRecipes.smelting().addSmelting(Blocks.oreMithril.blockID, new ItemStack(Items.oreMithrilItem), 0.1F);
-        FurnaceRecipes.smelting().addSmelting(Blocks.oreOrichalcum.blockID, new ItemStack(Items.oreOrichalcumItem), 0.1F);
+        FurnaceRecipes.smelting().addSmelting(Items.oreCopperItem.itemID , new ItemStack(Items.ingotCopperItem), 0.1F);
+        FurnaceRecipes.smelting().addSmelting(Items.oreIronItem.itemID, new ItemStack(Items.ingotIronItem), 0.2F);
+        FurnaceRecipes.smelting().addSmelting(Items.oreSilverItem.itemID, new ItemStack(Items.ingotSilverItem), 0.2F);
+        FurnaceRecipes.smelting().addSmelting(Items.oreGoldItem.itemID, new ItemStack(Items.ingotGoldItem), 0.2F);
+        FurnaceRecipes.smelting().addSmelting(Items.orePlatinumItem.itemID, new ItemStack(Items.ingotPlatinumItem), 0.3F);
+        FurnaceRecipes.smelting().addSmelting(Items.oreMithrilItem.itemID, new ItemStack(Items.ingotMithrilItem), 0.3F);
+        FurnaceRecipes.smelting().addSmelting(Items.oreOrichalcumItem.itemID, new ItemStack(Items.ingotOrichalcumItem), 0.8F);
         
-        //GameRegistry.addRecipe(new ItemStack(Items.toolCopperPickaxeItem), new Object[] {"", "", ""});
+        ItemStack enchanted = new ItemStack(Items.toolIronPickaxeItem);
+        enchanted.addEnchantment(Enchantment.aquaAffinity, 1);
         
+        GameRegistry.addShapelessRecipe(enchanted, new Object[] {
+                new ItemStack(Items.toolIronPickaxeItem), new ItemStack(Item.bucketWater)}); 
+        
+        ItemStack enchanted1 = new ItemStack(Items.toolIronPickaxeItem);
+        enchanted1.addEnchantment(Enchantment.efficiency, 1);
+        
+        GameRegistry.addShapelessRecipe(enchanted1, new Object[] {
+                new ItemStack(Items.toolIronPickaxeItem), new ItemStack(Item.appleRed)}); 
+            
+       
     }
-
+    
+    
 }
